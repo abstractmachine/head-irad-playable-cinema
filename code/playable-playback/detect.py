7c8caa66c7047fd5f27c3181bb4bc0bffacaeff6
abstractmachine <douglas@abstractmachine.net>
2025-06-17 16:23:54 +0200

    Better python pyenv instructions with dependencies. Better playback.py instructions

--- code/playable-playback/detect.py
+++ code/playable-playback/detect.py
@@ -1,6 +1,6 @@
 from ultralytics import YOLO
 
 model = YOLO("model.pt")  # your locally trained model
-for result in model("inside.mp4", stream=True):
+for result in model("video.mp4", stream=True):
 	result.show()          # shows frame with bounding boxes
 	
